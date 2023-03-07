d1eef99a6464c83053e047e84c24b51223c01241
colledwards <colledwards@expediagroup.com>
2023-03-07 10:03:20 -0800

    added remaining crud operations

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const {Restaurant} = require("./models/index")
 const {sequelize} = require("./db");
 
 const port = 3000;
-
+app.use(express.json());
 //TODO: Create your GET Request Route Below: 
 
 app.get("/restaurants", async (request, response) => {
@@ -17,6 +17,34 @@ app.get("/restaurants/:id", async (req, res) => {
     foundRestaurant = await Restaurant.findByPk(req.params.id);
 
     res.json(foundRestaurant);
+})
+
+app.post("/restaurants", async (req, res) => {
+    newRestaurant = await Restaurant.create({
+        name: req.body.name,
+        location: req.body.location,
+        cuisine: req.body.cuisine
+    });
+
+    res.json(newRestaurant);
+})
+
+app.put("/restaurants/:id", async (req, res) => {
+    foundRestaurant = await Restaurant.findByPk(req.params.id);
+    await foundRestaurant.update({
+        name: req.body.name,
+        location: req.body.location,
+        cuisine: req.body.cuisine
+    });
+
+    res.json(foundRestaurant);
+})
+
+app.delete("/restaurants/:id", async (req, res) => {
+    foundRestaurant = await Restaurant.findByPk(req.params.id);
+    await foundRestaurant.destroy();
+
+    res.send("Deleted!");
 })
 
 
