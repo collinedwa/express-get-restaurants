be36d6cbc9f157b6117c40cbb1e69e6f20213d96
colledwards <colledwards@expediagroup.com>
2023-03-08 10:46:19 -0800

    added validation

--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const {Restaurant} = require("../models/index")
+const {check, validationResult} = require("express-validator");
+const {Restaurant} = require("../models/index");
 const {sequelize} = require("../db");
 
 const restaurantRoutes = express.Router();
@@ -16,14 +17,23 @@ restaurantRoutes.get("/:id", async (req, res) => {
     res.json(foundRestaurant);
 })
 
-restaurantRoutes.post("/", async (req, res) => {
-    newRestaurant = await Restaurant.create({
-        name: req.body.name,
-        location: req.body.location,
-        cuisine: req.body.cuisine
-    });
+restaurantRoutes.post("/", [
+check("name").notEmpty().trim().isLength({min: 10, max:30}),
+check("location").notEmpty().trim(),
+check("cuisine").notEmpty().trim()],
+async (req, res) => {
+    const errors = validationResult(req);
 
-    res.json(newRestaurant);
+    if (!errors.isEmpty()){ 
+        res.json({error: errors.array()});
+    }else{
+        newRestaurant = await Restaurant.create({
+            name: req.body.name,
+            location: req.body.location,
+            cuisine: req.body.cuisine
+        });
+        res.json(newRestaurant);
+    }
 })
 
 restaurantRoutes.put("/:id", async (req, res) => {
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const {check, validationResult} = require("express-validator");
 const app = express();
 const {Restaurant} = require("./models/index")
 const {sequelize} = require("./db");
 const restaurantRoutes = require("./routes/restaurantRoutes");
+
 
 const port = 3000;
 app.use(express.json());
